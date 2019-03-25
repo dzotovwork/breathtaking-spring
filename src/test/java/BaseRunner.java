2a5eac9e388ca0a9900e446ba9d37e67b3b5e7a2
dmitry <dzotovwork@yandex.ru>
2019-03-25 09:07:26 +0300

    add baserunner add assert

---
+++ src/test/java/BaseRunner.java
@@ -0,0 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseRunner {
+    WebDriver driver;
+    String baseUrl;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        baseUrl = "https://www.tinkoff.ru/career/vacancies/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,35 +1,20 @@
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
-public class FirstTest {
-    private WebDriver driver;
-    private String baseUrl;
-    @Before
-    public void setUp(){
-        driver = new ChromeDriver();
-        baseUrl = "https://www.tinkoff.ru/career/vacancies/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
+public class FirstTest extends BaseRunner {
+
     @Test
-    public void testFirst(){
+    public void testFirst() {
         driver.get(baseUrl);
         driver.findElement(By.xpath("//input[@name='name']")).click();
         driver.findElement(By.xpath("//input[@name='name']")).sendKeys("AlohaVida");
         driver.findElement(By.xpath("//input[@name='name']")).sendKeys(Keys.TAB);
-        assertEquals("Допустимо использовать только буквы русского алфавита и дефис",driver.findElement(By.xpath("(//div[contains(@class,'ui-form-field-error-message')])[1]")).getText());
+        assertEquals("Допустимо использовать только буквы русского алфавита и дефис",
+                driver.findElement(By.xpath("(//div[contains(@class,'ui-form-field-error-message')])[1]")).getText());
+        assertEquals("Поле обязательное",
+                driver.findElement(By.xpath("(//div[contains(@class,'ui-form-field-error-message')])[2]")).getText());
     }
-    @After
-    public void tearDown(){
-        driver.quit();
-    }
-
 }
