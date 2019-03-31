94939504669d98c04258f1d564b1b914e8f19346
Dmitry <dzotovwork@yandex.ru>
2019-03-31 11:15:41 +0300

    add listener

--- src/test/java/BaseRunner.java
+++ src/test/java/BaseRunner.java
@@ -1,24 +1,37 @@
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseRunner {
-    WebDriver driver;
+    public static ThreadLocal<WebDriver> threadLocal = new ThreadLocal<>();
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public final String browserName = "chrome";
     String baseUrl;
 
     @Before
-    public void setUp() {
-        driver = BrowsersFactory.chrome.create();
-        driver.manage().window().maximize();
+    public void start() {
+        threadLocal = new ThreadLocal<>();
+        driver = new EventFiringWebDriver(getDriver());
+        ((EventFiringWebDriver) driver).register(new BrowsersFactory.MyListener());
+        threadLocal.set(driver);
         baseUrl = "https://www.tinkoff.ru/career/vacancies/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
 
 
     @After
     public void tearDown() {
         driver.quit();
+    }
+
+    private WebDriver getDriver() {
+        return BrowsersFactory.buildDriver(browserName);
     }
 }
--- src/test/java/BrowsersFactory.java
+++ src/test/java/BrowsersFactory.java
@@ -1,23 +1,62 @@
-import org.openqa.selenium.WebDriver;
+import com.google.common.io.Files;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 
-public enum BrowsersFactory {
-    chrome {
-        public WebDriver create() {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-notification");
-            return new ChromeDriver(options);
-        }
-    },
-    firefox {
-        public WebDriver create() {
-            return new FirefoxDriver();
-        }
-    };
+import java.io.File;
+import java.io.IOException;
 
-    public WebDriver create() {
-        return null;
+public class BrowsersFactory {
+    public static class MyListener extends AbstractWebDriverEventListener {
+
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println("Обращение к элементу " + by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println("Найден элемент " + by);
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            File tmp = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            File file = new File("target", "sccreen-" + System.currentTimeMillis() + ".png");
+            try {
+                Files.copy(tmp, file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.err.println(file.getAbsolutePath());
+        }
+    }
+
+    static WebDriver buildDriver(String browserName) {
+        switch (browserName) {
+
+            case "chrome_invisible":
+                ChromeOptions chromeInvisibleOpt = new ChromeOptions();
+                chromeInvisibleOpt.addArguments("--disable-notifications");
+                chromeInvisibleOpt.addArguments("--headless");
+                return new ChromeDriver(chromeInvisibleOpt);
+
+            case "firefox":
+                //Disable login to console and redirect log to an external file
+                System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+                System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "./src/test/java/firefox_logs/log");
+
+                FirefoxOptions ffOpt = new FirefoxOptions();
+                ffOpt.addPreference("dom.webnotifications.enabled", false);
+                return new FirefoxDriver(ffOpt);
+
+            default:
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--disable-notifications");
+                return new ChromeDriver(options);
+        }
     }
 }
